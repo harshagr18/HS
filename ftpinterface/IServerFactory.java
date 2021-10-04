6cc5f63ddd3ecaed407fc218c8c03af2b5d8605f
harshagr18 <harshagrawal1802@gmail.com>
2021-10-04 20:52:37 -0300

    Uploading Fixes and tests to IServerFactory

--- ftpinterface/IServerFactory.java
+++ ftpinterface/IServerFactory.java
@@ -8,3 +8,42 @@ public interface IServerFactory extends Remote {
 	public IServer createServer() throws RemoteException;
 
 }
+
+package hello;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GreeterTest {
+  
+  private Greeter greeter = new Greeter();
+
+  @Test
+  public void greeterSaysHello() {
+    assertThat(greeter.sayHello(), containsString("Hello"));
+  }
+
+}
+
+<dependencies>
+		<dependency>
+			<groupId>joda-time</groupId>
+			<artifactId>joda-time</artifactId>
+			<version>2.9.2</version>
+		</dependency>
+</dependencies>
+
+package hello;
+
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    LocalTime currentTime = new LocalTime();
+    System.out.println("The current local time is: " + currentTime);
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }
+}
